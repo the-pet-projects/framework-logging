1661ca62033832a2986ab5b6ddc507808da51b5d
joao.reis <joao.reis@farfetch.com>
2017-11-16 20:49:16 +0000

    fixed test

--- test/Logging.Producer.Tests/UnitTest1.cs
+++ test/Logging.Producer.Tests/UnitTest1.cs
@@ -1,6 +1,5 @@
 namespace PetProjects.Framework.Logging.Producer.Tests
 {
-    using Microsoft.Extensions.Logging;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -9,7 +8,6 @@ namespace PetProjects.Framework.Logging.Producer.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            new MessagingLoggingProvider().CreateLogger("asd").LogCritical("asd", "asd");
         }
     }
 }
