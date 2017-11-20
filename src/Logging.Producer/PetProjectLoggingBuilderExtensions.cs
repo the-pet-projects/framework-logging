3ca7c9ae6c10a9a64e5cf2be3e2305b76dbd8eee
joao.reis <joao.reis@farfetch.com>
2017-11-20 20:59:19 +0000

    added minimum loglevel to serilog

--- src/Logging.Producer/PetProjectLoggingBuilderExtensions.cs
+++ src/Logging.Producer/PetProjectLoggingBuilderExtensions.cs
@@ -3,10 +3,11 @@
     using System;
     using Microsoft.Extensions.Logging;
     using Serilog;
+    using Serilog.Events;
 
     public static class PetProjectLoggingBuilderExtensions
     {
-        public static ILoggingBuilder AddPetProjectLogging(this ILoggingBuilder builder, PeriodicSinkConfiguration sinkConfig, KafkaConfiguration kafkaConfig, bool dispose = false)
+        public static ILoggingBuilder AddPetProjectLogging(this ILoggingBuilder builder, LogEventLevel minLevel, PeriodicSinkConfiguration sinkConfig, KafkaConfiguration kafkaConfig, bool dispose = false)
         {
             if (builder == null)
             {
@@ -14,6 +15,7 @@
             }
 
             var logger = new LoggerConfiguration()
+                .MinimumLevel.Is(minLevel)
                 .Enrich.FromLogContext()
                 .WriteTo.Kafka(sinkConfig, kafkaConfig)
                 .CreateLogger();
